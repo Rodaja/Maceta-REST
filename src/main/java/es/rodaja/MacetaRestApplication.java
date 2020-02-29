fccca3f3f732c1adf7e6d7c89d81e19a29002050
Javi <javierplazasisques@gmail.com>
2020-02-29 13:46:51 +0100

    Root commit

---
+++ src/main/java/es/rodaja/MacetaRestApplication.java
@@ -0,0 +1,13 @@
+package es.rodaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacetaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacetaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/rodaja/MacetaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.rodaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacetaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
