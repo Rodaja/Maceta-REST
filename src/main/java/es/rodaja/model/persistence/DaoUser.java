20ce886474eb51fe076ec02be37e6eccfbbbbe69
Javi <javierplazasisques@gmail.com>
2020-02-29 14:51:54 +0100

    Find a user by email created

--- src/main/java/es/rodaja/model/persistence/DaoUser.java
+++ src/main/java/es/rodaja/model/persistence/DaoUser.java
@@ -1,9 +1,12 @@
 package es.rodaja.model.persistence;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.rodaja.model.entity.User;
 
 public interface DaoUser extends JpaRepository<User, Integer> {
 
+	public Optional<User> findByEmail();
 }
