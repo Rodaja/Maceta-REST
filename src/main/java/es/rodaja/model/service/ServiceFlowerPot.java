ba3395463e137a3dab989b325573a4e5636b8477
Javi <javierplazasisques@gmail.com>
2020-02-29 18:11:54 +0100

    Created method that saves a flowerpot

--- src/main/java/es/rodaja/model/service/ServiceFlowerPot.java
+++ src/main/java/es/rodaja/model/service/ServiceFlowerPot.java
@@ -1,8 +1,47 @@
 package es.rodaja.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import es.rodaja.model.entity.FlowerPot;
+import es.rodaja.model.persistence.DaoFlowerPot;
 
 @Service
 public class ServiceFlowerPot {
+	
+	@Autowired
+	private DaoFlowerPot dfp;
+	
+	/**
+	 * This method persist a flowerpot <b>only if it has the mac address not empty</b>
+	 * @param fp The flowerpot to be persist
+	 * @return True if the flowepot has been persisted, false in the opposite case
+	 */
+	public boolean save(FlowerPot fp) {
+		boolean response = false;
+		
+		if (checkFlowerPot(fp)) {
+			dfp.save(fp);
+			response = true;
+		}
+		
+		return response;
+	}
+
+	/**
+	 * This method checks if the mac address is not empty
+	 * @param fp The flowerpot to be checked
+	 * @return True if the mac address is not empty, false in the opposite case
+	 */
+	private boolean checkFlowerPot(FlowerPot fp) {
+		boolean response = false;
+		String macAddress = fp.getMacAddress();
+		
+		if (!macAddress.isEmpty()) {
+			response = true;
+		}
+		
+		return response;
+	}
 
 }
