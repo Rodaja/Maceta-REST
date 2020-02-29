61b972fea718596257a96ce3e994598f38e82ff3
Javi <javierplazasisques@gmail.com>
2020-02-29 14:23:41 +0100

    Entities User and Flowerpot created

--- src/main/java/es/rodaja/model/entity/FlowerPot.java
+++ src/main/java/es/rodaja/model/entity/FlowerPot.java
@@ -1,10 +1,77 @@
 package es.rodaja.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "flowerpot")
 public class FlowerPot {
 
+	@Id
+	private String macAddress;
+	private int groundHumidity; 
+	private int brightness;
+	private int airHumidity;
+	private int airTemperature;
+	
+	public FlowerPot() {
+	}
+	
+	public FlowerPot(String macAddress, int groundHumidity, int brightness, int airHumidity, int airTemperature) {
+		super();
+		this.macAddress = macAddress;
+		this.groundHumidity = groundHumidity;
+		this.brightness = brightness;
+		this.airHumidity = airHumidity;
+		this.airTemperature = airTemperature;
+	}
+
+	public String getMacAddress() {
+		return macAddress;
+	}
+
+	public void setMacAddress(String macAddress) {
+		this.macAddress = macAddress;
+	}
+
+	public int getGroundHumidity() {
+		return groundHumidity;
+	}
+
+	public void setGroundHumidity(int groundHumidity) {
+		this.groundHumidity = groundHumidity;
+	}
+
+	public int getBrightness() {
+		return brightness;
+	}
+
+	public void setBrightness(int brightness) {
+		this.brightness = brightness;
+	}
+
+	public int getAirHumidity() {
+		return airHumidity;
+	}
+
+	public void setAirHumidity(int airHumidity) {
+		this.airHumidity = airHumidity;
+	}
+
+	public int getAirTemperature() {
+		return airTemperature;
+	}
+
+	public void setAirTemperature(int airTemperature) {
+		this.airTemperature = airTemperature;
+	}
+
+	@Override
+	public String toString() {
+		return "FlowerPot [macAddress=" + macAddress + ", groundHumidity=" + groundHumidity + ", brightness="
+				+ brightness + ", airHumidity=" + airHumidity + ", airTemperature=" + airTemperature + "]";
+	}
+	
+	
 }
--- src/main/java/es/rodaja/model/entity/User.java
+++ src/main/java/es/rodaja/model/entity/User.java
@@ -1,10 +1,101 @@
 package es.rodaja.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String email;
+	private String name;
+	private String firstSurname;
+	private String secondSurname;
+	private String password;
+	private String country;
+
+	public User() {
+	}
+
+	public User(String email, String name, String firstSurname, String secondSurname, String password, String country) {
+		super();
+
+		this.email = email;
+		this.name = name;
+		this.firstSurname = firstSurname;
+		this.secondSurname = secondSurname;
+		this.password = password;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstSurname() {
+		return firstSurname;
+	}
+
+	public void setFirstSurname(String firstSurname) {
+		this.firstSurname = firstSurname;
+	}
+
+	public String getSecondSurname() {
+		return secondSurname;
+	}
+
+	public void setSecondSurname(String secondSurname) {
+		this.secondSurname = secondSurname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", email=" + email + ", name=" + name + ", firstSurname=" + firstSurname
+				+ ", secondSurname=" + secondSurname + ", password=" + password + ", country=" + country + "]";
+	}
+	
+	
 
 }
