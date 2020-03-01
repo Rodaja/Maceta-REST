380f0185b94f731c1602ff465d94b1ec643edd6e
Javi <javierplazasisques@gmail.com>
2020-03-01 20:40:12 +0100

    Created DELETE mapping delete a flowerpot

--- src/main/java/es/rodaja/controller/ControllerFlowerPot.java
+++ src/main/java/es/rodaja/controller/ControllerFlowerPot.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,17 @@ public class ControllerFlowerPot {
 			return new ResponseEntity<FlowerPot>(HttpStatus.NOT_FOUND);
 		}
 
+	}
+	
+	@DeleteMapping(path = "api/usuarios/{macAddress}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> borrarMacetaUsuario(@PathVariable("macAddress") String macAddress) {
+		Optional<FlowerPot> flowerpot = sf.findByMacAddress(macAddress);
+
+		if (flowerpot.isPresent()) {
+			sf.delete(macAddress);
+			return new ResponseEntity<String>("Deleted", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("NOT Deleted",HttpStatus.NOT_FOUND);
+		}
 	}
 }
