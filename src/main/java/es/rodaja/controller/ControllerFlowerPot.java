c4811d4b6db2ca1a4c822bd078751c3675240957
Javi <javierplazasisques@gmail.com>
2020-03-01 20:35:50 +0100

    Created PUT mapping to modify a flowerpot

--- src/main/java/es/rodaja/controller/ControllerFlowerPot.java
+++ src/main/java/es/rodaja/controller/ControllerFlowerPot.java
@@ -1,13 +1,16 @@
 package es.rodaja.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +38,16 @@ public class ControllerFlowerPot {
 		return new ResponseEntity<List<FlowerPot>>(flowerpots, HttpStatus.OK);
 	}
 
+	@PutMapping(path = "api/flowerpots/{macAddress}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<FlowerPot> addMacetaUsuario(@PathVariable("macAddress") String macAddress, @RequestBody FlowerPot flowerpotModified) {
+		Optional<FlowerPot> flowerpot = sf.findByMacAddress(macAddress);
+
+		if (flowerpot.isPresent()) {
+			sf.modify(flowerpotModified);
+			return new ResponseEntity<FlowerPot>(flowerpotModified, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<FlowerPot>(HttpStatus.NOT_FOUND);
+		}
+
+	}
 }
