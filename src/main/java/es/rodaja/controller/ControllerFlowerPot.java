abc6b76ff237bb52529bae4b0e46e48564651c97
Javi <javierplazasisques@gmail.com>
2020-03-01 20:29:53 +0100

    Created POST mapping to add a flowerpot

--- src/main/java/es/rodaja/controller/ControllerFlowerPot.java
+++ src/main/java/es/rodaja/controller/ControllerFlowerPot.java
@@ -1,8 +1,30 @@
 package es.rodaja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.rodaja.model.entity.FlowerPot;
+import es.rodaja.model.service.ServiceFlowerPot;
 
 @RestController
 public class ControllerFlowerPot {
+	
+	@Autowired
+	private ServiceFlowerPot sf;
+	
+	@PostMapping(path = "api/flowerpots", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<FlowerPot> guardar(@RequestBody FlowerPot fp) {
+		if (sf.save(fp)) {
+			return new ResponseEntity<FlowerPot>(fp, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<FlowerPot>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
 
 }
