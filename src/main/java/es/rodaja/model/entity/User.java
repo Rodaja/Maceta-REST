7e5397065e9f1357603062e266dd30a39a74d717
Javi <javierplazasisques@gmail.com>
2020-03-11 01:02:23 +0100

    Added unique in columns email and api key

--- src/main/java/es/rodaja/model/entity/User.java
+++ src/main/java/es/rodaja/model/entity/User.java
@@ -3,6 +3,7 @@ package es.rodaja.model.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,12 +18,14 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(nullable = false, unique= true)
 	private String email;
 	private String name;
 	private String firstSurname;
 	private String secondSurname;
 	private String password;
 	private String country;
+	@Column(unique= true)
 	private String apiKey;
 
 	@OneToMany(cascade = CascadeType.PERSIST)
