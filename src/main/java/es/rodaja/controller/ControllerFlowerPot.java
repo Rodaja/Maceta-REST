f3348ac11b39cf06821a3ccb02b62c374d4c009e
Javi <javierplazasisques@gmail.com>
2020-05-19 21:56:40 +0200

    OTA created

--- src/main/java/es/rodaja/controller/ControllerFlowerPot.java
+++ src/main/java/es/rodaja/controller/ControllerFlowerPot.java
@@ -1,9 +1,15 @@
 package es.rodaja.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -59,6 +65,41 @@ public class ControllerFlowerPot {
 			return new ResponseEntity<FlowerPot>(HttpStatus.NOT_FOUND);
 		}
 		
+	}
+	
+	@GetMapping(path = "api/flowerpots/update", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<Resource> updateSystem() {
+		File file = new File("/Users/javi/Desktop/code.bin");
+		
+		InputStreamResource resource = null;
+		try {
+			resource = new InputStreamResource(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(file.getAbsolutePath());
+		
+		String contentType = null;
+//        try {
+//            contentType = file.getAbsolutePath();
+//        } catch (Exception e) {
+//            System.out.println("Could not determine file type.");
+//        }
+		
+
+       
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        System.out.println(contentType);
+        
+        return ResponseEntity.ok()
+        .contentLength(file.length())
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .body(resource);
 	}
 
 	
